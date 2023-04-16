373eadabe110d02980b344009f20c327d9e282f6
Vladimir <limtis2109@gmail.com>
2023-04-17 01:20:20 +0200

    Changed division operator for consistency

--- src/loss/binary_crossentropy.py
+++ src/loss/binary_crossentropy.py
@@ -8,9 +8,8 @@ class BinaryCrossentropy(Loss):
         y_pred_clipped = np.clip(y_prediction, 1e-7, 1 - 1e-7)
 
         sample_losses = -(y_actual * np.log(y_pred_clipped) + (1 - y_actual) * np.log(1 - y_pred_clipped))
-        sample_losses = np.mean(sample_losses, axis=-1)
 
-        return sample_losses
+        return np.mean(sample_losses, axis=-1)
 
     def backward(self, d_values, y_actual):
         samples = len(d_values)
@@ -23,4 +22,4 @@ class BinaryCrossentropy(Loss):
         self.d_inputs = -(y_actual / d_values_clipped - (1 - y_actual) / (1 - d_values_clipped)) / labels
 
         # Normalize
-        self.d_inputs = self.d_inputs / samples
+        self.d_inputs /= samples
